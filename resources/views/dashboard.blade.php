04c57812f477ef7f3e4cd47aa42b7527198292ec
Ashok Mehta <78004850+ashok0425@users.noreply.github.com>
2025-04-20 14:50:02 +0545

    update detail

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,12 @@
 @extends('layout.master')
+@php
+$totalBlog=App\Models\Blog::query()->count();
+$totalWard=App\Models\Business::query()->count();
+$totalCategory=App\Models\Category::query()->count();
+$totalUser=App\Models\User::query()->count();
+
+@endphp
+
 @section('main-content')
     <style>
         .card {
@@ -15,6 +23,60 @@
 
 
 <div class="container-fluid p-0">
+    @php
+    $hour = now()->hour;
+    if ($hour < 12) {
+        $greeting = 'Good Morning';
+    } elseif ($hour < 17) {
+        $greeting = 'Good Afternoon';
+    } else {
+        $greeting = 'Good Evening';
+    }
+@endphp
+
+<div class="alert alert-success bg-success text-white p-2">
+    {{ $greeting }}  &nbsp;<strong>{{Auth::user()->name}} </strong>, Welcome Back!
+</div>
+@if (Auth::user()->can('do:anything'))
+
+    <div class="row">
+        <div class="col-md-3">
+            <div class="card bg-info shadow-sm">
+                <div class="card-body  text-white">
+                    <p style="font-size: 25px">Total Blog</p>
+                    <p style="font-size:20px">{{ $totalBlog }}</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card bg-secondary shadow-sm">
+                <div class="card-body  text-white">
+                    <p style="font-size: 25px">Total Ward</p>
+                    <p style="font-size:20px">{{ $totalWard }}</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card bg-success shadow-sm">
+                <div class="card-body  text-white">
+                    <p style="font-size: 25px">Total Category</p>
+                    <p style="font-size:20px">{{ $totalCategory }}</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card bg-warning shadow-sm">
+                <div class="card-body  text-white">
+                    <p style="font-size: 25px">Total Team</p>
+                    <p style="font-size:20px">{{ $totalUser }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endif
 
 
 </div>
@@ -22,18 +84,3 @@
 
 @endsection
 
-@push('scripts')
-<script
-type="text/javascript"
-src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"
-></script>
-<script
-type="text/javascript"
-src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"
-></script>
-<script>
-    $(document).ready(function () {
-        $('#dates').daterangepicker();
-        });
-</script>
-@endpush
--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -175,6 +175,7 @@
   </div>
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="{{asset('admin/js/app.js')}}"></script>
 
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
