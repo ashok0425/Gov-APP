846716cd26afab14b2349cadc0f875cdfebf369f
Ashok Mehta <78004850+ashok0425@users.noreply.github.com>
2025-06-08 16:02:59 +0545

    chai=nge admin email

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,7 +26,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         Gate::before(function ($user, $ability) {
-            if (in_array($user->email, ['barbardiyamunicipality@gmail.com']) ) {
+            if (in_array($user->email, ['yashukc41@gmail.com']) ) {
                 return true;
             }
             return null;
