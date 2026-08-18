2e55539a7b0e9167c24bb4d81c8eb389f3bae095
Ashok Mehta <ashokmehta1234y@gmail.com>
2026-08-18 20:41:41 +0545

    Split the sidebar: category, post, general
    The menu tree had grown to four entries sitting under a "Manage Post"
    header, which is not what they are. Categories get a section of their own,
    Manage Post holds the thing that gets filed into them, and Notification
    moves down to General beside Pages and Cms.
    
    The Post tab also highlights again: it was watching /blogs, and the posts
    resource has answered at /posts since it was named.

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -59,59 +59,62 @@
        @endcan --}}
 
         <ul class="sidebar-nav">
+            {{-- The menu tree has a section of its own; posts are what gets
+                 filed into it. --}}
+            @can('do:anything')
+            <li class="sidebar-header">Manage Category</li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link {{Request::is('categories','categories/*')?'text-light':' '}}" href="{{ route('categories.index') }}">
+                    <i class="fas fa-shopping-cart"></i>
+                    <span class="align-middle">Category</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link {{Request::is('subcategories','subcategories/*')?'text-light':' '}}" href="{{ route('subcategories.index') }}">
+                    <i class="fas fa-sitemap"></i>
+                    <span class="align-middle">Subcategory</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link {{Request::is('child-categories','child-categories/*')?'text-light':' '}}" href="{{ route('childcategories.index') }}">
+                    <i class="fas fa-stream"></i>
+                    <span class="align-middle">Child Category</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link {{Request::is('grandchild-categories','grandchild-categories/*')?'text-light':' '}}" href="{{ route('grandchildcategories.index') }}">
+                    <i class="fas fa-code-branch"></i>
+                    <span class="align-middle">Grandchild Category</span>
+                </a>
+            </li>
+            @endcan
+
+            @canAny(['post:view','post:create','post:edit','post:delete'])
             <li class="sidebar-header">Manage Post</li>
 
-          @can('do:anything')
-          <li class="sidebar-item">
-            <a class="sidebar-link {{Request::is('categories','categories/*')?'text-light':' '}}" href="{{ route('categories.index') }}">
-                <i class="fas fa-shopping-cart"></i>
-                <span class="align-middle">Category</span>
-            </a>
-        </li>
-
-        <li class="sidebar-item">
-            <a class="sidebar-link {{Request::is('subcategories','subcategories/*')?'text-light':' '}}" href="{{ route('subcategories.index') }}">
-                <i class="fas fa-sitemap"></i>
-                <span class="align-middle">Subcategory</span>
-            </a>
-        </li>
-
-        <li class="sidebar-item">
-            <a class="sidebar-link {{Request::is('child-categories','child-categories/*')?'text-light':' '}}" href="{{ route('childcategories.index') }}">
-                <i class="fas fa-stream"></i>
-                <span class="align-middle">Child Category</span>
-            </a>
-        </li>
-
-        <li class="sidebar-item">
-            <a class="sidebar-link {{Request::is('grandchild-categories','grandchild-categories/*')?'text-light':' '}}" href="{{ route('grandchildcategories.index') }}">
-                <i class="fas fa-code-branch"></i>
-                <span class="align-middle">Grandchild Category</span>
-            </a>
-        </li>
-        @endcan
-
-
-        @canAny(['post:view','post:create','post:edit','post:delete'])
-        <li class="sidebar-item">
-            <a class="sidebar-link {{Request::is('blogs','blogs/*')?'text-light':' '}}" href="{{ route('blogs.index') }}">
-                <i class="fas fa-photo-video"></i>
-                <span class="align-middle">Post</span>
-            </a>
-        </li>
-          @endcanAny
-
-        @can('do:anything')
-        <li class="sidebar-item">
-            <a class="sidebar-link {{Request::is('notices','notices/*')?'text-light':' '}}" href="{{ route('notices.index') }}">
-                <i class="fas fa-bell"></i>
-                <span class="align-middle">Notification</span>
-            </a>
-        </li>
-        @endcan
+            <li class="sidebar-item">
+                {{-- The posts resource answers at /posts, whatever its route
+                     name says, so that is what marks the tab as open. --}}
+                <a class="sidebar-link {{Request::is('posts','posts/*')?'text-light':' '}}" href="{{ route('blogs.index') }}">
+                    <i class="fas fa-photo-video"></i>
+                    <span class="align-middle">Post</span>
+                </a>
+            </li>
+            @endcanAny
 
             @can('do:anything')
-          <li class="sidebar-header">General</li>
+            <li class="sidebar-header">General</li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link {{Request::is('notices','notices/*')?'text-light':' '}}" href="{{ route('notices.index') }}">
+                    <i class="fas fa-bell"></i>
+                    <span class="align-middle">Notification</span>
+                </a>
+            </li>
 
             <li class="sidebar-item">
                 <a class="sidebar-link {{Request::is('pages','pages/*')?'text-light':' '}}" href="{{ route('pages.index') }}">
@@ -121,13 +124,12 @@
             </li>
 
             <li class="sidebar-item">
-                <a class="sidebar-link {{Request::is('websites','websites/*')?'text-light':' '}}" href="{{ route('cms.edit', 1) }}">
+                <a class="sidebar-link {{Request::is('cms','cms/*')?'text-light':' '}}" href="{{ route('cms.edit', 1) }}">
                     <i class="fas fa-images"></i>
                     <span class="align-middle">Cms</span>
                 </a>
             </li>
             @endcan
-
 
         </ul>
     </div>
