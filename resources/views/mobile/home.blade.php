0ff8577c8fbe0a9cf080620609b44077a6d3ec0f
Ashok Mehta <ashokmehta1234y@gmail.com>
2026-08-20 10:26:17 +0545

    The news heading gets its own megaphone artwork

--- resources/views/mobile/home.blade.php
+++ resources/views/mobile/home.blade.php
@@ -69,7 +69,7 @@
     @endif
 
     <h2 class="section-title section-title-lead">
-        <span class="material-symbols-rounded">campaign</span>
+        <img src="{{ asset('mobile/img/megaphone.png') }}" alt="" width="22" height="22">
         भर्खरका मुख्य सूचनाहरू
     </h2>
 
